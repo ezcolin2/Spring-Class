163a9e3e62bc1d6abad74e2c392ef7c0506d624d
하하 <105545215+ezcolin2@users.noreply.github.com>
2023-02-12 20:22:31 +0900

    Create JwtUtil.java

---
+++ jwt/JwtUtil.java
@@ -0,0 +1,14 @@
+public class JwtUtil {
+    public static boolean isExpired(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().getExpiration().before(new Date());
+    }
+    public static String createJwt(String secretKey, Long expiredMs) {
+        String token = Jwts.builder()
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+        return token;
+    }
+}
