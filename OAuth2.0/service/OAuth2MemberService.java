471be943dc409fd18791b2f0a1e566c0241592bc
ez <105545215+ezcolin2@users.noreply.github.com>
2023-04-02 00:45:41 +0900

    Create OAuth2MemberService.java
    oauth2 로그인 및 Repository에 저장

---
+++ OAuth2.0/service/OAuth2MemberService.java
@@ -0,0 +1,47 @@
+@Service
+@RequiredArgsConstructor
+public class OAuth2MemberService extends DefaultOAuth2UserService {
+    private final BCryptPasswordEncoder encoder;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        OAuth2MemberInfo memberInfo = null;
+        System.out.println(oAuth2User.getAttributes());
+        System.out.println(userRequest.getClientRegistration().getRegistrationId());
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        if (registrationId.equals("google")) {
+            memberInfo = new GoogleMemberInfo(oAuth2User.getAttributes());
+        } else if (registrationId.equals("facebook")) {
+            memberInfo = new FacebookMemberInfo(oAuth2User.getAttributes());
+        } else if (registrationId.equals("naver")) {
+            memberInfo = new NaverMemberInfo((Map)oAuth2User.getAttributes().get("response"));
+        } else {
+            System.out.println("로그인 실패");
+        }
+        String provider = memberInfo.getProvider();
+        String providerId = memberInfo.getProviderId();
+        String username = provider + "_" + providerId; //중복이 발생하지 않도록 provider와 providerId를 조합
+        String email = memberInfo.getEmail();
+        String role = "ROLE_ADMIN"; //일반 유저
+        System.out.println(oAuth2User.getAttributes());
+        Optional<Member> findMember = memberRepository.findByName(username);
+        Member member=null;
+        if (findMember.isEmpty()) { //찾지 못했다면
+            member = Member.builder()
+                    .name(username)
+                    .email(email)
+                    .password(encoder.encode("password"))
+                    .role(role)
+                    .provider(provider)
+                    .providerId(providerId).build();
+            memberRepository.save(member);
+        }
+        else{
+            member=findMember.get();
+        }
+        return new PrincipalDetails(member, oAuth2User.getAttributes());
+    }
+}
