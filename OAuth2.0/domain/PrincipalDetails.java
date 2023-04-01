8cd30c6a876d5f1a6377cc5a41e8326a247a1521
ez <105545215+ezcolin2@users.noreply.github.com>
2023-04-02 00:43:09 +0900

    Create PrincipalDetails.java
    일반 로그인과 oauth2 로그인 통합

---
+++ OAuth2.0/domain/PrincipalDetails.java
@@ -0,0 +1,62 @@
+@Getter
+public class PrincipalDetails implements OAuth2User , UserDetails {
+    private Member member;
+    private Map<String, Object> attributes;
+
+    public PrincipalDetails(Member member) {
+        this.member=member;
+    }
+
+    public PrincipalDetails(Member member, Map<String, Object> attributes) {
+        this.member=member;
+        this.attributes=attributes;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return member.getRole();
+            }
+        });
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "name";
+    }
+}
