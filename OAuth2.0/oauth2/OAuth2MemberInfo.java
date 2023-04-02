ed4c9a50c7bfe9d62d9300cd138515a303dfc490
ez <105545215+ezcolin2@users.noreply.github.com>
2023-04-03 01:42:51 +0900

    Create OAuth2MemberInfo.java

---
+++ OAuth2.0/oauth2/OAuth2MemberInfo.java
@@ -0,0 +1,6 @@
+public interface OAuth2MemberInfo {
+    String getProviderId(); //공급자 아이디 ex) google, facebook
+    String getProvider(); //공급자 ex) google, facebook
+    String getName(); //사용자 이름 ex) 홍길동
+    String getEmail(); //사용자 이메일 ex) gildong@gmail.com
+}
