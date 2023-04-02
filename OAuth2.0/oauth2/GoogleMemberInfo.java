f7cf8b3bfe7ca4584a86403bd4a742a500fccf37
ez <105545215+ezcolin2@users.noreply.github.com>
2023-04-03 01:43:44 +0900

    Create GoogleMemberInfo.java

---
+++ OAuth2.0/oauth2/GoogleMemberInfo.java
@@ -0,0 +1,26 @@
+public class GoogleMemberInfo implements OAuth2MemberInfo{
+    public GoogleMemberInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    private Map<String, Object> attributes;
+    @Override
+    public String getProviderId() {
+        return (String) attributes.get("sub");
+    }
+
+    @Override
+    public String getProvider() {
+        return "google";
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+}
