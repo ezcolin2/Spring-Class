d76f865e235ee220d986d0494f7def3f2ea168f7
ez <105545215+ezcolin2@users.noreply.github.com>
2023-04-03 01:43:08 +0900

    Create FacebookMemberInfo.java

---
+++ OAuth2.0/oauth2/FacebookMemberInfo.java
@@ -0,0 +1,26 @@
+public class FacebookMemberInfo implements OAuth2MemberInfo{
+    private Map<String, Object> attributes;
+    @Override
+    public String getProviderId() {
+        return (String) attributes.get("id");
+    }
+
+    public FacebookMemberInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProvider() {
+        return "facebook";
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+}
